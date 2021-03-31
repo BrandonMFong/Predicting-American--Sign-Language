590bbbd1eb86d8ae1a9b207677c96cd74f87807e
BrandonMFong <fong.m.brandon97@gmail.com>
2021-03-30 17:21:59 -0700

    init project

---
+++ main.py
@@ -0,0 +1,18 @@
+# Brando 
+# 3/30/2021
+# Final Project
+
+class Project():
+
+    _description =  "Final Project: \n" \
+                    "Author: Brando \n" \
+                    "Description: \n" \
+                    "This is my project"   
+
+    def Description(self):
+        print(self._description)
+
+if __name__ == "__main__":
+    project = Project()
+    
+    project.Description()
